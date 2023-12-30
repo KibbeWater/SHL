b3f224a47323e69e0f14f1de94111ce5e30c041a
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2023-12-30 19:12:41 +0100

    Initial Commit

---
+++ LHF/LHFApp.swift
@@ -0,0 +1,17 @@
+//
+//  LHFApp.swift
+//  LHF
+//
+//  Created by user242911 on 12/30/23.
+//
+
+import SwiftUI
+
+@main
+struct LHFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
