e30da209eeb11f9f2408c62f33750790df280418
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2024-03-21 23:51:24 +0100

    Added new match overview component

---
+++ LHF/Components/GameDisplays/PrevMatch.swift
@@ -0,0 +1,18 @@
+//
+//  PrevMatch.swift
+//  LHF
+//
+//  Created by user242911 on 3/21/24.
+//
+
+import SwiftUI
+
+struct PrevMatch: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PrevMatch()
+}
