a22fee00012cc78890cb41505276c24f6a233815
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2024-03-27 20:35:38 +0100

    Fixes and debug options

--- SHL/Components/DebugView.swift
+++ SHL/Components/DebugView.swift
@@ -9,25 +9,34 @@ import SwiftUI
 import HockeyKit
 
 struct DebugView: View {
+    @EnvironmentObject var matchInfo: MatchInfo
+    
     var body: some View {
         VStack {
-            HStack {
-                ScrollView {
-                    Text(Logging.shared.getLogs())
+            ForEach(matchInfo.latestMatches.filter(IsLive), id: \.id) { match in
+                Button("\(match.homeTeam.name) v. \(match.awayTeam.name)") {
+                    Task {
+                        do {
+                            if let overview = try await matchInfo.getMatch(match.id) {
+                                try ActivityUpdater.shared.start(match: overview)
+                            }
+                        } catch let err {
+                            print("Error fetching match")
+                            print(err)
+                        }
+                    }
                 }
-                Spacer()
             }
-            Spacer()
-            Button("Download") {
-                
-            }
-            .buttonStyle(.borderedProminent)
-            
         }
         .padding()
+    }
+    
+    func IsLive(_ game: Game) -> Bool {
+        return !game.played && game.date < Date.now
     }
 }
 
 #Preview {
     DebugView()
+        .environmentObject(MatchInfo())
 }
